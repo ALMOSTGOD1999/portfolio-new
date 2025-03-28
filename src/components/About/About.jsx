4633982d98cf41312c8582fbd42a106914dd4b8f
Rahul Mukherjee <139303113+ALMOSTGOD1999@users.noreply.github.com>
2025-03-28 23:50:45 +0530

    okay

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -18,19 +18,19 @@ const About = () => {
         className={styles["about-me-long"]}
         variants={fadeIn("", "", 0.1, 1)}
       >
-        I hold a degree in Computer Science and Business Systems from Sister
-        Nivedita University. I have a passion for software development, web
-        development, and a keen interest in learning new technologies and
-        frameworks. As a Web Developer Intern at TCS iON, I completed two
-        projects that demonstrated my skills in web development. I also gained
-        experience working with the MERN Stack, Python, REST APIs, Redux, Git,
-        and Docker. Currently, I work at Cloudifyapps as a Frontend Developer,
-        where I focus on building responsive and user-friendly interfaces using
-        React. I value user satisfaction, quality, and innovation in my work,
-        and I am always eager to take on new challenges and opportunities to
-        grow as both a developer and a team player. My goal is to leverage my
-        skills and knowledge to create impactful, user-friendly digital
-        solutions for various industries.
+        Identifying myself as a coding-entusiast coming from Civil Engineering
+        field has largely shaped my perspectives and aspirations. I understand
+        how it feels to be in a new field at the age of 23 and starting learning
+        new things and dealing with financial/food insecurity or without
+        opportunity and go above and beyond when an opportunity arises. I take
+        great pride in persistence, diligence, and empathy. This is why I love
+        coding and its functions as societal tools that allow
+        individuals/corporations to overcome hurdles and accelerate growth. I
+        also credit my resourceful working style for my love of guitar. Life has
+        taught me to seek out opportunities, whether or not they are risky.
+        Please feel free to contact me via LinkedIn or email. I'm always looking
+        forward to an insightful conversation over coffee, or even better, a
+        round of paragliding.
       </motion.p>
       <div className={styles["service-card"]}>
         {serviceData.map((data, index) => (
